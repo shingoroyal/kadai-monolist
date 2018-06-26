955171c8957004f90c2d5623d26f1422bf41d568
EC2 Default User <ec2-user@ip-172-31-47-27.us-east-2.compute.internal>
2018-06-26 04:06:55 +0000

    include have button

--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -16,6 +16,8 @@
                             <div class="buttons text-center">
                                 @if (Auth::check())
                                     @include('items.want_button', ['item' => $item])
+                                     @include('items.have_button', ['item' => $item])
+                                    
                                 @endif
                             </div>
                         </div>
